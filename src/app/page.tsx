1a61749ec5fcccf3fa9e6b24b14ef6a2caa551b5
Simon Morgan <simon.morgan80@gmail.com>
2025-09-08 16:57:24 +0100

    content: copy updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,65 @@ import Hero from '@/app/ui/hero';
 import TextBlock from '@/app/ui/text-block';
 
 export default function Home() {
-  return (
-    <div className="">
-      <Hero title="Website Development">
-        <p>Available for contracts starting July 2025</p>
-      </Hero>
+    return (
+        <div className="">
+            <Hero title="Website Development">
+                <p>Available for contracts starting August 2025</p>
+            </Hero>
 
-      <div className="container mx-auto py-10 px-4">
-        <div className="grid lg:grid-cols-12 gap-4 lg:gap-8">
-          <div className="col-span-8">
-
-            <TextBlock title=""> 
-              <p>Freelance front-end web developer and technical lead with over 17 years agency experience. I specialise in creating responsive, accessible and high performing front end user interfaces using HTML, CSS and Javascript.</p>                          
-              <p>Experience of developing and working with Design Systems & Component libraries using tools such as <a href="https://storybook.js.org/" target="_blank" rel="noreferrer">Storybook.js</a> and <a href="https://fractal.build/" target="_blank" rel="noreferrer">Fractal.build</a></p>
-              <p>Extensive experience of developing large custom component based CMS websites using Wordpress and Advanced Custom Fields including 3rd party API integration. Knowledge of Headless CMS platforms such as <a href="https://prismic.io/" target="_blank" rel="noopener noreferrer">Prismic</a> and <a href="https://www.sanity.io/" target="_blank" rel="noopener noreferrer">Sanity</a></p>
-              <p>Javascript experience ranging from vanilla js, web components to <a href="https://react.dev/" target="_blank">React</a> as well as knowledge of <a href="https://nextjs.org/" target="blank">Next.js</a>, <a href="https://svelte.dev/" target="blank">Svelte</a>, Typescript</p> 
-              <p>CSS experience of native to SASS, CSS Modules and other frameworks such as Tailwind.</p>
-            </TextBlock>
-
-          </div>
-          <div className="col-span-4">
-            <ContactCard />
-          </div>
+            <div className="container mx-auto py-10 px-4">
+                <div className="grid lg:grid-cols-12 gap-4 lg:gap-8">
+                    <div className="col-span-8">
+                        <TextBlock title="">
+                            <p>
+                                Senior web developer and technical lead with
+                                over 17 years agency experience. I specialise in
+                                creating responsive, accessible and high
+                                performing front end user interfaces, component
+                                libraries and CMS websites.
+                            </p>
+                            <h2 className="text-1xl lg:text-2xl font-medium mb-5">
+                                Skills
+                            </h2>
+                            <ul>
+                                <li className="mb-3">
+                                    <strong>Javascript</strong> - Vanilla,
+                                    React, Next, Typescript
+                                </li>
+                                <li className="mb-3">
+                                    <strong>CSS</strong> - SASS, BEM, Tailwind,
+                                    Modules
+                                </li>
+                                <li className="mb-3">
+                                    <strong>Backend</strong> - PHP, mySQL, Node,
+                                    Express
+                                </li>
+                                <li className="mb-3">
+                                    <strong>CMS</strong> - Wordpress + knowledge
+                                    of Headless systems such as Sanity, Prismic
+                                    and Contentful
+                                </li>
+                                <li className="mb-3">
+                                    <strong>SSG</strong> - Next, Gatsby, Astro,
+                                    11ty
+                                </li>
+                                <li className="mb-3">
+                                    <strong>Design systems</strong> -
+                                    Storybook.js, Fractal.build
+                                </li>
+                                <li>
+                                    <strong>Tools</strong> - GIT, Gulp, Webpack,
+                                    Vite, Npm, Kinsta, AWS, Bitbucket CI
+                                    Pipelines
+                                </li>
+                            </ul>
+                        </TextBlock>
+                    </div>
+                    <div className="col-span-4">
+                        <ContactCard />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-
-    </div>
-  );
+    );
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -7,17 +7,18 @@ const projects = [
         name: 'VIPA Digital',
         url: 'https://vipadigital.com',
         date: 'June 2025 - July 2025',
-        content: '<p>Working directly with client to redevelop website based on updated brand/redesign.  Moved to Wordpress CMS built using ACF blocks and <a href="https://vite.dev/" target="_blank">Vite</a> Front end tooling</p>',
-        subprojects: [
-            'https://vipadigital.com'
-        ]
+        content:
+            '<p>Working directly with client to redevelop website based on updated brand/redesign.  Moved to Wordpress CMS built using ACF blocks and <a href="https://vite.dev/" target="_blank">Vite</a> Front end tooling</p>',
+        subprojects: ['https://vipadigital.com'],
     },
     {
         name: 'Bladonmore',
         url: '',
-        content: '<p>Contract working for <a href="https://www.bladonmore.com" target="_blank" rel="noreferrer noopener">Bladonmore.com</a></p><p>Development of custom Wordpress builds using Advanced Custom Fields,  HTML, CSS, Javascript, Webpack & Gulp</p>',
+        content:
+            '<p>Contract working for <a href="https://www.bladonmore.com" target="_blank" rel="noreferrer noopener">Bladonmore.com</a></p><p>Development of custom Wordpress builds using Advanced Custom Fields,  HTML, CSS, Javascript, Webpack & Gulp</p>',
         date: 'Oct 2022 - June 2025 <br>June 2021 - Jan 2022',
         subprojects: [
+            'https://www.first-quantum.com/',
             'https://www.ritcap.com/',
             'https://www.arapartners.com/',
             'https://www.royaltypharma.com/',
@@ -26,74 +27,79 @@ const projects = [
             'https://www.stonepeak.com/',
             'https://www.bladonmore.com',
             'https://www.arrowfletchings.net/',
-            'https://www.aac-clyde.space/'
-        ]
+            'https://www.aac-clyde.space/',
+        ],
     },
     {
         name: 'Havas Lynx Faze',
         url: '',
-        content: '<p>React JS development, Gatsby and Wordpress site support </p>',
-        date: 'Jun 2022 - Sep 2022'
-    },    
+        content:
+            '<p>React JS development, Gatsby and Wordpress site support </p>',
+        date: 'Jun 2022 - Sep 2022',
+    },
     {
         name: 'Pixeledeggs',
         url: '',
-        content: '<p>Contract working for <a href="https://www.pixeledeggs.com" target="_blank" rel="noreferrer noopener">Pixeledeggs.com</a></p><p>Part of development team developing custom Wordpress builds using Advanced Custom Fields</p>',
+        content:
+            '<p>Contract working for <a href="https://www.pixeledeggs.com" target="_blank" rel="noreferrer noopener">Pixeledeggs.com</a></p><p>Part of development team developing custom Wordpress builds using Advanced Custom Fields</p>',
         date: 'June 2020 - Dec 2020',
         subprojects: [
             'https://www.missingpeople.org.uk/',
             'https://www.immersivearcade.uk',
-            'https://www.earthshotprize.org'
-        ]
+            'https://www.earthshotprize.org',
+        ],
     },
     {
         name: 'xdefi.io',
         url: 'https://www.xdefi.io',
-        content: '<p>Custom Wordpress build using Advanced Custom Fields,  HTML, CSS, Javascript, Webpack & Gulp</p><p>Note - site updated since my involvement</p>',
-        date: 'May 2021'
+        content:
+            '<p>Custom Wordpress build using Advanced Custom Fields,  HTML, CSS, Javascript, Webpack & Gulp</p><p>Note - site updated since my involvement</p>',
+        date: 'May 2021',
     },
     {
         name: 'rafaelvalls.co.uk',
         url: 'https://www.rafaelvalls.co.uk/',
-        content: '<p>Custom Wordpress build for Art Gallery using Advanced Custom Fields,  HTML, CSS, Javascript, Webpack & Gulp</p>',
+        content:
+            '<p>Custom Wordpress build for Art Gallery using Advanced Custom Fields,  HTML, CSS, Javascript, Webpack & Gulp</p>',
         date: 'May 2020',
-        subprojects: [
-            'https://www.rafaelvalls.co.uk/'
-        ]
-    },    
+        subprojects: ['https://www.rafaelvalls.co.uk/'],
+    },
     {
         name: 'living-group.com',
         url: '',
-        content: '<p>Contract working for <a href="http://www.living-group.com" target="_blank" rel="noreferrer noopener">Living Group</a></p><p>Front End Component library development for Umbraco CMS sites. HTML, CSS, Javascript, Fractal.js, Webpack, Gulp. </p>',
+        content:
+            '<p>Contract working for <a href="http://www.living-group.com" target="_blank" rel="noreferrer noopener">Living Group</a></p><p>Front End Component library development for Umbraco CMS sites. HTML, CSS, Javascript, Fractal.js, Webpack, Gulp. </p>',
         date: 'Multiple projects between 2017 - 2021',
         subprojects: [
             'https://www.burfordcapital.com/',
             'https://www.potterclarkson.com/',
             'https://www.traverssmith.com/',
             'https://www.harneys.com/',
-            'https://www.kasowitz.com/'
-        ]
+            'https://www.kasowitz.com/',
+        ],
     },
     {
         name: 'elselondon.com',
         url: '',
-        content: '<p>Contracts working for <a href="http://www.elselondon.com" target="_blank" rel="noreferrer noopener">Elselondon</a></p><p>Wordpress Build + Front End Component library development. Wordpress, HTML, CSS, Javascript, Fractal.js, Webpack, Gulp.</p>',
+        content:
+            '<p>Contracts working for <a href="http://www.elselondon.com" target="_blank" rel="noreferrer noopener">Elselondon</a></p><p>Wordpress Build + Front End Component library development. Wordpress, HTML, CSS, Javascript, Fractal.js, Webpack, Gulp.</p>',
         date: 'Multiple projects between 2018 - 2021',
         subprojects: [
             'https://www.elselondon.com/',
-            'https://www.englandgolf.org/'
-        ]
+            'https://www.englandgolf.org/',
+        ],
     },
     {
         name: 'theteam.co.uk',
         url: '',
-        content: '<p>Contract working for <a href="http://www.theteam.co.uk" target="_blank" rel="noreferrer noopener">The Team</a></p><p>Front End Component library development. HTML, CSS, Javascript, Fractal.js, Webpack, Gulp plus Custom Wordpress builds</p>',
+        content:
+            '<p>Contract working for <a href="http://www.theteam.co.uk" target="_blank" rel="noreferrer noopener">The Team</a></p><p>Front End Component library development. HTML, CSS, Javascript, Fractal.js, Webpack, Gulp plus Custom Wordpress builds</p>',
         date: 'Multiple projects between 2018 - 2022',
         subprojects: [
             'https://www.theteam.co.uk',
             'https://www.nsandi.com',
             'https://www.nsandi-corporate.com/',
-        ]
+        ],
     },
 ];
 
@@ -111,8 +117,8 @@ export default function Portfolio() {
                                 <div key={index}>
                                     <Project project={project} />
                                 </div>
-                            )
-                        })}                        
+                            );
+                        })}
                     </div>
                     <div className="col-span-4">
                         <ContactCard />
